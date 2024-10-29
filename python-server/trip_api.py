fa67402db800894d63323ac416637ffcefb89c23
asfi50 <www.asfi50@gmail.com>
2024-10-29 21:18:03 +0600

    trip api change

--- python-server/trip_api.py
+++ python-server/trip_api.py
@@ -19,11 +19,15 @@ import os
 import random
 load_dotenv()
 
+# todays date
+from datetime import date
+today = date.today().strftime("%d/%m/%Y")
+
 
 app=FastAPI(
     title="Easy Trip API Server",
     version="1.0",
-    description="API Server of Easy Trip Planner"
+    description="API Server of Easy Trip Planner",
 )
 
 app.add_middleware(
@@ -53,7 +57,7 @@ def get_hotels_tool(place: str) -> str:
 
 ## Prompt Template
 trip_data_extract_prompt=ChatPromptTemplate.from_messages(
-   [ ("system","""you are good at extracting data from natural language. your response will be in pure JSON format. Example Response Structure:
+   [ ("system",f"""you are good at extracting data from natural language. your response will be in pure JSON format. Example Response Structure:
         'origin': 'Dhaka',
         'destination': 'Sajek',
         'days': 5,
@@ -61,7 +65,7 @@ trip_data_extract_prompt=ChatPromptTemplate.from_messages(
         'people': 2,
         'preferences': 'hill',
         'tripType': 'oneWay' | 'roundTrip' | 'multiCity',
-        'journeyDate': today Date,
+        'journeyDate': {today},
         'travelClass': 'economy' | 'business' | 'luxury',
      """),
      ("user","extract the data from this input: {trip_text}. default value for tripType is oneWay, journeyDate is today, travelClass is economy. all data will be in string. dont use null value for any field,use blank string if not available.")]
@@ -69,7 +73,7 @@ trip_data_extract_prompt=ChatPromptTemplate.from_messages(
 
 trip_plan_prompt=ChatPromptTemplate.from_messages(
     [
-       ("system", """You are a good trip planner specialized in Bangladeshi trips and provide accurate data. You will provide the response in pure JSON format. Example Response Structure:
+       ("system", f"""You are a good trip planner specialized in Bangladeshi trips and provide accurate data. You will provide the response in pure JSON format. Example Response Structure:
             'trip_name': 'Dhaka to Sajek 5 days trip with 2 people to enjoy the hill',
             'origin': 'Dhaka',
             'destination': 'Sajek',
@@ -78,7 +82,7 @@ trip_plan_prompt=ChatPromptTemplate.from_messages(
             'people': '2',
             'preferences': 'hill',
             'tripType': 'oneWay',
-            'journeyDate': '24/10/2024',
+            'journeyDate': {today},
             'travelClass': 'economy',
             'checkpoints': [ 
                 'origin': 
@@ -117,6 +121,46 @@ trip_plan_prompt=ChatPromptTemplate.from_messages(
             ],
             'food': 
                 '1': 
+                'breakfast':
+                            "title": "Montana Restaurant",
+                            "address": "97MV M73, Sajek",
+                            "latitude": 23.384133499999997,
+                            "longitude": 92.2931734,
+                            "rating": 4.1,
+                            "ratingCount": 168,
+                            "category": "Restaurant",
+                            "phoneNumber": "01879-086308",
+                            "cid": "2530207382346985444",
+                            "website": "https://montanarestaurant.com/"
+                            "cost": "200"
+                        ,
+                'launch':
+                            "title": "Kashbon Restaurant",
+                            "address": "97PR 8M4, Z1603, Sajek",
+                            "latitude": 23.385738699999997,
+                            "longitude": 92.2916992,
+                            "rating": 4.4,
+                            "ratingCount": 40,
+                            "category": "Restaurant",
+                            "phoneNumber": "01648-676555",
+                            "website": "http://facebook.com/kashbonsajek",
+                            "cid": "13782344097276060861"
+                            'cost': '250'
+                        ,
+                'dinner':
+                        "title": "Sajek Chilekotha Restaurant",
+                        "address": "Dighinala - Sajek Rd, Sajek",
+                        "latitude": 23.383672400000002,
+                        "longitude": 92.2933472,
+                        "rating": 4,
+                        "ratingCount": 25,
+                        "category": "Restaurant",
+                        "phoneNumber": "01862-000810",
+                        "cid": "14729805586170578036"
+                        'cost': '550'
+                        
+                ,
+                 '2': 
                 'breakfast':
                             "title": "Montana Restaurant",
                             "address": "97MV M73, Sajek",
@@ -213,7 +257,14 @@ trip_plan_prompt=ChatPromptTemplate.from_messages(
                 'accommodation': '2000',
                 'miscellaneous': '100'
             """),
-       ("user","create a trip plan for {origin} to {destination} for {days} days with {people} people and budget {budget} taka. preferences are {preferences}. tripType is {tripType} and journeyDate is {journeyDate}. travelClass is {travelClass} and must use these hotel search results: {hotels} for accommodation. must use hotels from {restaurants} list. determine the cost based on the rating as bangladeshi standard, if rating not given determine by own. default value for tripType is oneWay, journeyDate is today, travelClass is economy. all values will in string. use blank instead of null")
+       ("user","create a trip plan for {origin} to {destination} for {days} days with {people} people and budget {budget} taka. preferences are {preferences}. tripType is {tripType} and journeyDate is {journeyDate}. travelClass is {travelClass} and must use these hotel search results: {hotels} for accommodation. must use hotels from {restaurants} list also all days should have breakfast launch and dinner minimum. determine the cost based on the rating as bangladeshi standard, if rating not given determine by own. default value for tripType is oneWay, journeyDate is today, travelClass is economy. all values will in string. use blank instead of null")
+    ]
+)
+
+qna_prompt=ChatPromptTemplate.from_messages(
+    [
+       ("system", "You are a QnA model specialized in Bangladeshi trips and provide accurate data based on the data given to you"),
+       ("user","question: {question} . context: {context}")
     ]
 )
 
@@ -223,8 +274,7 @@ llm_gpt = ChatOpenAI(
     temperature=0,
     max_tokens=None,
     timeout=None,
-    max_retries=2,
-    api_key= os.getenv("OPENAI_API_KEY")
+    max_retries=2
 )
 output_parser=StrOutputParser()
 json_output_parser=JSONOutputParser()
@@ -240,6 +290,12 @@ add_routes(
     app,
     trip_plan_prompt|llm_gpt|json_output_parser,
     path="/trip_plan",
+)
+
+add_routes(
+    app,
+    qna_prompt|llm_gpt|output_parser,
+    path="/qna",
 )
 
 
